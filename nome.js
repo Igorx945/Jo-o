b45fd53ab46cf96440f16b4eef24850c3643a589
Igorx945 <128699017+Igorx945@users.noreply.github.com>
2024-02-23 11:49:37 -0300

    Add files via upload

---
+++ nome.js
@@ -0,0 +1,9 @@
+let nome ="igor";
+let idade = 17;
+let inteiro = 10;
+let altura = 1.56;
+let peso = 52;
+console.log(`o meu nome é ${nome} e tenho ${idade}`);
+console.log(`a idade é ${idade + inteiro} `);
+console.log(`Eu tenho ${altura} de altura e ${peso} kg.`);
+
